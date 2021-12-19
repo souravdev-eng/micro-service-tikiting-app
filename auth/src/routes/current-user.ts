a211496c0961cf8538c1ac10c01172b8e6033204
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2021-12-19 18:08:18 +0530

    Adding get current user information

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
-router.get('/api/users/currentUser', (req, res) => {
-  res.send('Current user...');
+router.get('/api/users/currentuser', (req, res) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.status(200).send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRoute };
