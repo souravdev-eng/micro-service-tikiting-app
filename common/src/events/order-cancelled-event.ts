90ec567e20b784198bea77a94bac3b4cb58cbef4
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2022-01-28 18:29:19 +0530

    Updates

---
+++ common/src/events/order-cancelled-event.ts
@@ -0,0 +1,11 @@
+import { Subjects } from "./subjects";
+
+export interface OrderCancelled {
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    title: {
+      id: string;
+    };
+  };
+}
